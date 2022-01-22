9b3ca3156dd2f3ca53c4c6f4bd339dd7c20c40ff
Saifurrahmanemon <saifemon789@gmail.com>
2022-01-22 19:16:12 +0600

    designed sorting-visualizer display box

--- src/SortingAlgorithms/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingAlgorithms/SortingVisualizer/SortingVisualizer.jsx
@@ -11,24 +11,28 @@ export default function SortingVisualizer() {
 
     const resetArray = () => {
         const array = [];
-        for (let i = 0; i < 30; i++) {
-            array.push(RandomValueGenerator(5, 100));
+        for (let i = 0; i < 20; i++) {
+            array.push(RandomValueGenerator(20, 300));
         }
         setArray(array);
     };
 
     return (
-        <div className="array-display-container">
+        <div className="visualizer-container">
             <h1>Algorithms visualizer</h1>
-            {array.map((value, index) => (
-                <div
-                    className="array-bar"
-                    key={index}
-                    style={{ height: `${value}px` }}
-                >
-                    <span className="value-inside-bar">{value}</span>
+            <div className="array-display-container">
+                <div className="array-bar-container">
+                    {array.map((value, index) => (
+                        <div
+                            className="array-bar"
+                            key={index}
+                            style={{ height: `${value}px` }}
+                        >
+                            <span className="value-inside-bar">{value}</span>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </div>
             <div className="algorithms-button">
                 <button onClick={() => resetArray()}>generate New Array</button>
             </div>
